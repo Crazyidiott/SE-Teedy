0fbf602c4d392893d273395acb82ac0907f65ad5
Crazyidiott <1073405983@qq.com>
2025-05-01 21:55:20 +0800

    update user register controller

---
+++ docs-web/src/main/webapp/src/app/docs/controller/Register.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * User registration controller.
+ */
+angular.module('docs').controller('Register', function($scope, $state, Restangular) {
+  $scope.user = {
+    username: '',
+    password: '',
+    email: '',
+    message: ''
+  };
+  
+  $scope.isSuccess = false;
+  $scope.isError = false;
+  
+  /**
+   * Register form submission.
+   */
+  $scope.register = function() {
+    $scope.isError = false;
+    $scope.errorUsername = false;
+    $scope.errorPassword = false;
+    $scope.errorEmail = false;
+    $scope.errorAlreadyExists = false;
+    $scope.errorRegistrationPending = false;
+    
+    Restangular.one('user/registration').put($scope.user).then(function() {
+      $scope.isSuccess = true;
+    }, function(e) {
+      $scope.isError = true;
+      if (e.data.type === 'ValidationError') {
+        if (e.data.message.indexOf('username') !== -1) {
+          $scope.errorUsername = true;
+        }
+        if (e.data.message.indexOf('password') !== -1) {
+          $scope.errorPassword = true;
+        }
+        if (e.data.message.indexOf('email') !== -1) {
+          $scope.errorEmail = true;
+        }
+      } else if (e.data.type === 'AlreadyExistingUsername') {
+        $scope.errorAlreadyExists = true;
+      } else if (e.data.type === 'RegistrationPending') {
+        $scope.errorRegistrationPending = true;
+      }
+    });
+  };
+});
